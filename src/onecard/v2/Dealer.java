9fa061f6fd5efc7ac87fb29fbd20ad2c70478e31
taewan625 <taewan1997625@gmail.com>
2023-03-30 10:08:09 +0900

    v2 Dealer & TDD
    selfFeedback에 세부사항 작성
    0. oneCardDeck 생성
    1. shuffle()
    2. giveCard()
    3. resetCard()

---
+++ src/onecard/v2/Dealer.java
@@ -0,0 +1,53 @@
+package onecard.v2;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Dealer {
+
+    // OneCardDeck과 같은 경우는 공유하는 부분들이 많아서 오히려 객체 생성 후 밖으로 빼는 것이 효율적이다.
+    // 또한 OneCardDeck은 deck으로써의 의미 밖에 없어서 Dealer말고는 사용하는 class가 없다.
+    OneCardDeck oneCardDeck = new OneCardDeck();
+
+    void shuffleOneCardDeck() {
+        LinkedList<OneCard> oneCardList = oneCardDeck.oneCardList;
+        Collections.shuffle(oneCardList);
+    }
+
+//    해당 giveCard는 실제 게임을 돌리게 되면 player에게 sublist해서 카드 주는 코드를 매됨 작성하게 된다.
+//    List<OneCard> giveCard(int giveNum) {
+//        LinkedList<OneCard> oneCardList = oneCardDeck.oneCardList;
+//        return oneCardList.subList(0, giveNum);
+//    }
+    void giveCard(Player player, int cards) {
+        // 중요 ->  매개변수를 잘 이용하면 class 안에 다른 class의 객체를 생성하지 않아도 해당 method처럼 활용이 가능하다.
+        LinkedList<OneCard> playerDeck = player.playerDeck;
+        List<OneCard> oneCards = oneCardDeck.oneCardList.subList(0, cards);
+        playerDeck.addAll(oneCards);
+
+        /**
+         * removeAll()에 oneCards 사용시 :
+         * removeAll() 메서드가 실행되는 동안 반복자가 여전히 해당 컬렉션의 요소를 탐색
+         * 컬렉션을 수정하면서 동시에 해당 컬렉션을 탐색하는 경우 발생할 수 있는 ConcurrentModificationException 발새
+         * 해결 :
+         * 복사본을 만들어서 복사본에서 removeAll() 수행
+         */
+        LinkedList copyOneCards = new LinkedList<>(oneCards);
+        oneCardDeck.oneCardList.removeAll(copyOneCards);
+    }
+
+
+    void resetCard(OpenDeck openDeck){
+        // openDeck의 마지막 카드를 제외하고 다 oneCardDeck에 주고 shuffle 하기
+        int size = openDeck.openDeckList.size();
+        List<OneCard> oneCards = openDeck.openDeckList.subList(0, size - 1);
+        oneCardDeck.oneCardList.addAll(oneCards);
+
+        LinkedList copyOneCards = new LinkedList<>(oneCards);
+        openDeck.openDeckList.removeAll(copyOneCards);
+        shuffleOneCardDeck();
+    }
+
+
+}
---
+++ test/onecard/v2/DealerTest.java
@@ -0,0 +1,107 @@
+package onecard.v2;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DealerTest {
+
+    Dealer dealer = new Dealer(); // shuffle 한 list
+    Dealer fixedDealer = new Dealer(); // shuffle 안한 list
+
+
+    boolean shuffle() {
+
+        LinkedList<OneCard> oneCardList = dealer.oneCardDeck.oneCardList;
+        dealer.shuffleOneCardDeck();
+        System.out.println("oneCardList = " + oneCardList);
+
+        LinkedList<OneCard> oneCardList1 = fixedDealer.oneCardDeck.oneCardList;
+        System.out.println("oneCardList1 = " + oneCardList1);
+
+        int same = 0;
+        for (int i = 0; i < 54; i++) {
+            boolean equals = Objects.equals(oneCardList.get(i), oneCardList1.get(i));
+            if (equals) same++;
+            if (same > 5) return false;
+        }
+        System.out.println("same = " + same);
+        return true;
+    }
+
+
+//    이전 버전의 dealer의 giveCard를 이용하게 되면 실제 게임 코드도 아래와 같이 많이 작성하게 된다.
+//    int playerDeck(int giveNum){ // 실제 게임에서는
+//        Player player = new Player(); // method() 밖으로 꺼내면 size()가 중첩 됨
+//        LinkedList<OneCard> playerDeck = player.playerDeck; -> 1. 생략됨
+//        playerDeck.addAll(dealer.giveCard(giveNum));        -> 2. 생략ㄴ
+//        return playerDeck.size();
+//    }
+    int playerDeck(int giveNum){ // 실제 게임에서는
+        Player player = new Player(); // method() 밖으로 꺼내면 size()가 중첩 됨
+        dealer.giveCard(player, giveNum);
+        return player.playerDeck.size();
+    }
+
+    int oneCardDeck(int giveNum){
+        Player player = new Player(); // method() 밖으로 꺼내면 size()가 중첩 됨
+        dealer.giveCard(player, giveNum);
+        return dealer.oneCardDeck.oneCardList.size();
+    }
+
+    OpenDeck openDeck = new OpenDeck();
+    boolean resetCard() {
+        LinkedList<OneCard> oneCardList = dealer.oneCardDeck.oneCardList;
+
+        List<OneCard> oneCards = oneCardList.subList(0, 52);
+        openDeck.openDeckList.addAll(oneCards);
+
+        LinkedList copyList = new LinkedList(oneCards);
+        oneCardList.removeAll(copyList);
+
+        System.out.println("dealer.oneCardDeck.oneCardList = " + dealer.oneCardDeck.oneCardList);
+        System.out.println("openDeck = " + openDeck.openDeckList);
+
+        OneCard last = openDeck.openDeckList.getLast();
+        dealer.resetCard(openDeck); // 핵심
+        OneCard oneCard = openDeck.openDeckList.get(0);
+
+        System.out.println("dealer.oneCardDeck.oneCardList1 = " + dealer.oneCardDeck.oneCardList);
+        System.out.println("openDeck1 = " + openDeck.openDeckList);
+        System.out.println("dealer.oneCardDeck.oneCardList.size() = " + dealer.oneCardDeck.oneCardList.size());
+
+        return last == oneCard;
+    }
+
+    @Test
+    void shuffleTest() {
+        assertTrue(shuffle()); // 동일한 위치에 동일한 카드가 5개 미만이면 true;
+    }
+
+    @Test
+    void playerDeckSizeTest() {
+        assertEquals(1, playerDeck(1));
+        assertEquals(7, playerDeck(7));
+        assertEquals(20, playerDeck(20));
+        assertEquals(26, playerDeck(26));
+    }
+
+    @Test
+    void oneCardDeckSizeTest(){
+        assertEquals(54, oneCardDeck(0));
+        assertEquals(53, oneCardDeck(1));
+        assertEquals(43, oneCardDeck(10));
+        assertEquals(33, oneCardDeck(10));
+        assertEquals(0, oneCardDeck(33));
+
+    }
+    @Test
+    void resetCardTest(){
+        assertTrue(resetCard());
+    }
+
+}
